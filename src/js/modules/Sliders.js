71a763d69b7f8f393bb970b66b8b677bd9c1ed1b
maksym-vovk <mvx.vovk@gmail.com>
2022-06-24 19:10:39 +0300

    Redesigned main comments and about block

--- src/js/modules/Sliders.js
+++ src/js/modules/Sliders.js
@@ -56,13 +56,27 @@ const Sliders = (function () {
 
       reviewsSlider.slick({
         arrows: false,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
         dots: true,
         infinite: false,
         customPaging: function (slick, index) {
           return `<a data-slide-index=${index + 1}>${index + 1}</a>`;
         },
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              adaptiveHeight: true
+            },
+          }]
       });
 
       reviewsSlider.on(
